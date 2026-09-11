bab7a32442fb922ca169190a823b7749a246be15
Subrata Pramanik <109057053+subrata-9999@users.noreply.github.com>
2026-09-11 16:40:00 +0530

    Time: 11 ms (84.40%) | Memory: 19.4 MB (38.46%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -0,0 +1,14 @@
+import itertools
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        count = 0
+        unique_perms = set(itertools.permutations(digits, 3))
+        even_numbers = [
+            p for p in unique_perms
+            if p[0] != 0          # Condition 1: Avoid starting with zero
+            and p[-1] % 2 == 0    # Condition 2: Must be an even number (ends in 0, 2, 4, 6, 8)
+        ]
+
+        return len(even_numbers)
+        
+        
